6162134d028019ed23095fa48b53bb3e47e99481
ZHIGUO ZHANG <54384930+zhangzg78@users.noreply.github.com>
2023-03-16 08:04:14 +0800

    Update subfunc_mwt.m

--- subfunc_mwt.m
+++ subfunc_mwt.m
@@ -13,7 +13,6 @@ function [P, S] = subfunc_mwt(x, f, Fs, omega, sigma)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % // Input // %
 % x:            the original data samples (Time Points x Channels)
-% xtimes:       the time axis of the original data
 % Fs:           sampling rate
 % omega:        a parameter to define the central frequency of Morlet wavelet
 % sigma:        a parameter to define the spread of Morlet wavelet in time domain
@@ -29,8 +28,8 @@ fprintf('Calculating Morlet Wavelet Transform ... ')
 if size(x,2)==1; x = x.'; end
 x = detrend(x,'linear'); % remove linear trends
 
-N_F = length(f); % number of time samples
-N_T = length(x); % number of frequency bins
+N_F = length(f); % number of frequency bins
+N_T = length(x); % number of time samples
 f = f/Fs;     % normalized frequency
 
 S = single(zeros(N_F,N_T)); % define the size of output 
